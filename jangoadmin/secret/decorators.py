827cc6813d24625778d8e0759ab5d5fe286229c3
rahulsps <rahul_bhola@softprodigy.com>
2020-03-13 12:43:14 +0530

    add comments

--- jangoadmin/secret/decorators.py
+++ jangoadmin/secret/decorators.py
@@ -2,25 +2,28 @@ from django.core.exceptions import PermissionDenied
 from django.contrib.auth.models import User
 from django.shortcuts import render,redirect
 
+# NOT IN USE CURRENTLY
 def my_login_required(function):
    def wrap(request, *args, **kwargs):
+      # check some condition here
       if request.session['name'] :
          return function(request, *args, **kwargs)
       else:
          return render(request, 'secret/login.html')
          raise PermissionDenied
    wrap.__doc__ = function.__doc__
-   wrap.__name__ = function.__name__   
+   wrap.__name__ = function.__name__
    return wrap
 
 
-
+# NOT IN USE CURRENTLY
 def superadmin_required(function):
    def wrap(request, *args, **kwargs):
+      # check some condition here
       if request.session['role'] == "SuperAdmin":
          return function(request, *args, **kwargs)
       else:
          raise PermissionDenied
    wrap.__doc__ = function.__doc__
-   wrap.__name__ = function.__name__   
+   wrap.__name__ = function.__name__
    return wrap
